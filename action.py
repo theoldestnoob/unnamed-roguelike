1d0de71e8a085ff3904d00ebecf7d46f029e9463
theoldestnoob <will.hinz@gmail.com>
2019-08-14 22:16:10 -0500

    prep for action system change to queue-based
    notes on current actions in dict-based system
    Action class for each action
    Actions enum for action types

---
+++ action.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug 14 22:06:21 2019
+
+@author: theoldestnoob
+"""
+
+from enum import Enum, auto
+
+
+class Action():
+    def __init__(self, ident, source=None, target=None, args=None):
+        self.ident = ident
+        self.source = source
+        self.target = target
+        self.args = args
+
+
+class Actions(Enum):
+    MESSAGE = auto()
+    MOVE = auto()
+    MOVE_ASTAR = auto()
+    MELEE = auto()
+    WAIT = auto()
+    SPAWN_ETHERIC = auto()
+    DESPAWN_ETHERIC = auto()
+    POSSESS = auto()
+    UNPOSSESS = auto()
+    PICKUP = auto()
+    ITEM_ADDED = auto()
+    USE_ITEM = auto()
+    CONSUMED = auto()
+    DROP_ITEM = auto()
+    ITEM_DROPPED = auto()
+    TARGETING = auto()
+    CANCEL_TARGET = auto()
+    DEAD = auto()
+    XP = auto()
+    LEVEL_UP = auto()
+    EQUIP = auto()
+    EQUIPPED = auto()
+    DEQUIPPED = auto()
+    EXIT = auto()
+    FULLSCREEN = auto()
+    MOUSEMOTION = auto()
+    MSG_UP = auto()
+    MSG_DOWN = auto()
+    SHOW_INVENTORY = auto()
+    DROP_INVENTORY = auto()
+    TAKE_STAIRS = auto()
+    SHOW_CHARACTER_SCREEN = auto()
+    OMNIVIS = auto()
+    SWITCH_CHAR = auto()
+    MAP_GEN = auto()
+    GRAPH_GEN = auto()
+    TEST = auto()
